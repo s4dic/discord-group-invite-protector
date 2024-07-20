bfd09d00b544c2555f9128a5848886893d199e5a
Sleek <42476966+s4dic@users.noreply.github.com>
2024-07-21 00:27:25 +0200

    Update index.js
    Ajout de la fonction de whitelist et blacklist, ajout en bonus de la fonction pour supprimer les messages envoyés

--- index.js
+++ index.js
@@ -2,35 +2,103 @@ const { Client } = require('discord.js-selfbot-v13');
 const fs = require('fs');
 const client = new Client();
 
-// Configure tes options ci-dessous
-const token = ""; // Remplacez par votre token ici
-const notificationGroupId = ""; // Remplacez par votre ID du groupe où notifier les invitations
-const leaveMessage = "🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕"; // Message à envoyer avant de quitter
-// Activation ou désactivation des paramètres
-const silentLeave = true; // Mettre à 'true' pour quitter silencieusement, 'false' pour quitter le canal avec notification
-const sendMessage = true; // Mettre à 'true' pour envoyer un message avant de quitter, 'false' pour ne pas envoyer de message
-const enableLogs = true; // Mettre à 'true' pour activer les logs, 'false' pour désactiver
-
-// Lire les exceptions de groupes depuis le fichier whitelist.txt
-const exceptions = fs.readFileSync('whitelist.txt', 'utf-8')
-    .split('\n')
-    .map(line => line.split('#')[0].trim()) // Ignorer les commentaires et les espaces
-    .filter(Boolean); // Filtrer les lignes vides
+//###################################################################################################### CONFIGURE TES OPTIONS CI-DESSOUS#
+const token = ""; // Mettre le token ici                                                                                                ##
+const notificationGroupId = ""; // ID du groupe où notifier les invitations                                                             ##
+const leaveMessage = "🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕"; // Message à envoyer avant de quitter              ##
+const rmCommand = "!rm"; // Commande pour supprimer les derniers messages [Tu peux mettre le mot que tu veux mais sans effacer le "!"]  ##
+const deleteMessageCount = 30; // Nombre de messages à supprimer par défaut [Ne pas depasser 100 pour eviter le ban]                    ##
+const deleteDelay = 30; // Délai de suppression en millisecondes                                                                        ##
+// Activation ou désactivation des paramètres                                                                                           ##
+const silentLeave = true; // Mettre à 'true' pour quitter silencieusement, 'false' pour quitter le canal avec notification              ##
+const sendMessage = true; // Mettre à 'true' pour envoyer un message avant de quitter, 'false' pour ne pas envoyer de message           ##
+const enableLogs = true; // Mettre à 'true' pour activer les logs, 'false' pour désactiver                                              ##
+//########################################################################################################################################
 
 let inviterMap = new Map();
+
+function loadWhitelist() {
+    return fs.readFileSync('whitelist.txt', 'utf-8')
+        .split('\n')
+        .map(line => line.split('#')[0].trim()) // Ignorer les commentaires et les espaces
+        .filter(Boolean); // Filtrer les lignes vides
+}
+
+function addToWhitelist(id, name = '') {
+    fs.appendFileSync('whitelist.txt', `${id} # ${name}\n`, 'utf-8');
+}
+
+function removeFromWhitelist(id) {
+    const whitelist = fs.readFileSync('whitelist.txt', 'utf-8').split('\n');
+    const updatedWhitelist = whitelist.filter(line => !line.startsWith(id));
+    fs.writeFileSync('whitelist.txt', updatedWhitelist.join('\n'), 'utf-8');
+}
+
+async function deleteMessages(channel, messages) {
+    for (const message of messages) {
+        try {
+            await channel.messages.delete(message.id); // Utiliser l'ID du message
+            await new Promise(resolve => setTimeout(resolve, deleteDelay)); // Utiliser le délai de suppression configuré
+        } catch (error) {
+            console.error(`Failed to delete message: ${error}`);
+        }
+    }
+}
 
 client.on('ready', async () => {
     if (enableLogs) {
         console.log(`Logged in as ${client.user.tag}`);
     }
     client.on('messageCreate', async (message) => {
-        if (message.channel.type === 'GROUP_DM' && !exceptions.includes(message.channel.id)) {
+        if (message.content.startsWith('!wl')) {
+            const args = message.content.split(' ').slice(1);
+            const id = args[0];
+            const name = args.slice(1).join(' ');
+
+            if (id) {
+                addToWhitelist(id, name);
+                message.channel.send(`Le groupe ${id} a été ajouté à la whitelist avec le nom ${name}.`);
+            }
+        }
+
+        if (message.content.startsWith('!uwl')) {
+            const args = message.content.split(' ').slice(1);
+            const id = args[0];
+
+            if (id) {
+                removeFromWhitelist(id);
+                message.channel.send(`Le groupe ${id} a été supprimé de la whitelist.`);
+            }
+        }
+
+        if (message.content.startsWith(rmCommand) && message.author.id === client.user.id) {
+            const args = message.content.split(' ').slice(1);
+            let count = deleteMessageCount;
+
+            if (args.length > 0) {
+                const arg = parseInt(args[0], 10);
+                if (!isNaN(arg) && arg > 0) {
+                    count = arg;
+                } else {
+                    message.channel.send(`Argument invalide pour la commande ${rmCommand}`);
+                    return;
+                }
+            }
+
+            const fetchedMessages = await message.channel.messages.fetch({ limit: 100 });
+            const myMessages = Array.from(fetchedMessages.filter(msg => msg.author.id === client.user.id).values()).slice(0, count);
+            await deleteMessages(message.channel, myMessages);
+        }
+
+        if (message.channel.type === 'GROUP_DM' && !loadWhitelist().includes(message.channel.id)) {
             if (!inviterMap.has(message.channel.id)) {
                 inviterMap.set(message.channel.id, message.author.id);
             }
         }
     });
+
     setInterval(async () => {
+        const exceptions = loadWhitelist();
         client.channels.cache.forEach(async (channel) => {
             if (channel.type === 'GROUP_DM') {
                 if (!exceptions.includes(channel.id)) {
@@ -40,7 +108,7 @@ client.on('ready', async () => {
                         }
                         await channel.delete(silentLeave);
 
-                        // Notifier dans le groupe spécifié l'ID de la personne qui a invité et la date/heure
+                        // Notifier dans le groupe spécifié l'ID de la personne qui a invité, la date/heure et l'ID du groupe
                         const inviterId = inviterMap.get(channel.id);
                         const notificationChannel = client.channels.cache.get(notificationGroupId);
                         if (notificationChannel) {
